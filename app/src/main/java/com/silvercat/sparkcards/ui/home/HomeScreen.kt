4f19d1863a2562ce4467969edc58df94e5459a46
ywy <you@example.com>
2026-08-15 21:20:16 +0800

    fix

--- app/src/main/java/com/silvercat/sparkcards/ui/home/HomeScreen.kt
+++ app/src/main/java/com/silvercat/sparkcards/ui/home/HomeScreen.kt
@@ -24,7 +24,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.pointerInput.pointerInput
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
